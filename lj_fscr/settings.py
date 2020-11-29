5374d1792e7468f9545fcc6a97961388244c1779
Lucyjpjones <lucyjpjones@gmail.com>
2020-11-29 10:14:32 +0000

    Added home app and templates

--- lj_fscr/settings.py
+++ lj_fscr/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
     # Other
     'storages',  # AWS
@@ -63,7 +64,10 @@ ROOT_URLCONF = 'lj_fscr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

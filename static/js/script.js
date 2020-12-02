513d2e6ed45b494f811259e663b681d91490f20d
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-02 22:03:21 +0000

    added css and js for innerheight vh

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
-
-/* Sidebar Toggle*/
+  /* Sidebar Toggle*/
   $("#sidebarCollapse").on("click", function () {
     $("#sidebar").toggleClass("active");
   });
 
-/* Menu icon change (toggle) - Code taken from 'https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close' */
+  /* Menu icon change (toggle) - Code taken from 'https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close' */
   $("#sidebarCollapse").on("click", function () {
     $("#navbar-bars").toggleClass("d-none");
     $("#navbar-close").toggleClass("d-none");
   });
-  
+
+  // We listen to the resize event
+  window.addEventListener("resize", () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
 });

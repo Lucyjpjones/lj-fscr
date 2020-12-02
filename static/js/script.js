3cad991906a1ae6d0188ac1f2f3f34a12ff43bb4
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-02 15:23:22 +0000

    media queries added, base template complete for mobile view

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
+
+/* Sidebar Toggle*/
   $("#sidebarCollapse").on("click", function () {
     $("#sidebar").toggleClass("active");
   });
 
-  /* https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close*/
-  
+/* Menu icon change (toggle) - Code taken from 'https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close' */
   $("#sidebarCollapse").on("click", function () {
     $("#navbar-bars").toggleClass("d-none");
     $("#navbar-close").toggleClass("d-none");
   });
+  
 });

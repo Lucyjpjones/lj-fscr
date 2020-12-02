28ced5370f5106a42fddfc6e0a376703bbefceaa
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-02 12:52:03 +0000

    menu moved to left side, collapse sidenav working, icon change js added

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,12 @@
+$(document).ready(function () {
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+  });
 
+  /* https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close*/
+  
+  $("#sidebarCollapse").on("click", function () {
+    $("#navbar-bars").toggleClass("d-none");
+    $("#navbar-close").toggleClass("d-none");
+  });
+});

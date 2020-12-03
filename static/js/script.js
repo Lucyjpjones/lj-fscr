b088d84fd6fceb38bc1c61e2ded3683adb31af3f
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-03 09:05:30 +0000

    amendments to json file content and models added for fixtures

--- static/js/script.js
+++ static/js/script.js
@@ -10,10 +10,4 @@ $(document).ready(function () {
     $("#navbar-close").toggleClass("d-none");
   });
 
-  // We listen to the resize event
-  window.addEventListener("resize", () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
 });

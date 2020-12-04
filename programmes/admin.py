e34165c600f4232b8f49b47a8f672d940c5a00f8
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-04 14:06:29 +0000

    Customised admin

--- programmes/admin.py
+++ programmes/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Programme, Category
 
-# Register your models here.
 
-admin.site.register(Programme)
-admin.site.register(Category)
+class ProgrammeAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Programme, ProgrammeAdmin)
+admin.site.register(Category, CategoryAdmin)

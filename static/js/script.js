ee734f854b7f32975de1478cee043a77d58f09e9
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-04 07:22:42 +0000

    Added js and css to change vh for mobile

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
-  /* Sidebar Toggle*/
-  $("#sidebarCollapse").on("click", function () {
-    $("#sidebar").toggleClass("active");
-  });
+    /* Sidebar Toggle*/
+    $("#sidebarCollapse").on("click", function () {
+        $("#sidebar").toggleClass("active");
+    });
 
-  /* Menu icon change (toggle) - Code taken from 'https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close' */
-  $("#sidebarCollapse").on("click", function () {
-    $("#navbar-bars").toggleClass("d-none");
-    $("#navbar-close").toggleClass("d-none");
-  });
+    /* Menu icon change (toggle) - Code taken from 'https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close' */
+    $("#sidebarCollapse").on("click", function () {
+        $("#navbar-bars").toggleClass("d-none");
+        $("#navbar-close").toggleClass("d-none");
+    });
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 });

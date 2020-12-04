ceab42f7f10210a9b115fc21286f1ba9c19b4617
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-04 20:13:58 +0000

    Meet the coaches template added, added to home views and urls. Tab functionality working and navigation added from homepage

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('meet_the_coaches', views.meet_the_coaches, name='meet_the_coaches'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def meet_the_coaches(request):
+    """ A view to return the coaches page """
+
+    return render(request, 'home/meet_the_coaches.html')

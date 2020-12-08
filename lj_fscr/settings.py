742f97d30e4980af42eb86807821b4a4b47801cb
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-08 18:48:33 +0000

    Completed confirmation emails

--- lj_fscr/settings.py
+++ lj_fscr/settings.py
@@ -211,3 +211,4 @@ if 'USE_AWS' in os.environ:
     STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
     STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
     STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+    DEFAULT_FROM_EMAIL = 'fscr@fscr.com'

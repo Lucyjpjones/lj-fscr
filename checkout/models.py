299ca4041b7d5ab3b88d5cb3bed11404a60a54a5
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-09 17:53:17 +0000

    Profile page formatted

--- checkout/models.py
+++ checkout/models.py
@@ -23,7 +23,7 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)

239c7f95d428aa7033ec03fa04cf6ff0bb81b5b7
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-09 10:49:36 +0000

    Email sending now working correctly

--- lj_fscr/settings.py
+++ lj_fscr/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-Debug = os.environ.get("DEVELOPMENT", False)
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lj-fscr.herokuapp.com', 'localhost']
 

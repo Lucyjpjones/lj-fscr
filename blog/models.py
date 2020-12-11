d99d18cdf03a42fbad83fc049f9460743391e796
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-11 15:04:41 +0000

    Comments added to blog post detail with styling

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
+# https://djangocentral.com/building-a-blog-application-with-django/
+
 from django.db import models
 from django.contrib.auth.models import User
 
-# https://djangocentral.com/building-a-blog-application-with-django/
 STATUS = (
     (0, "Draft"),
     (1, "Publish")
 )
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -23,3 +25,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import CommentForm
 from django.db.models.functions import Lower
 
 
@@ -33,12 +34,25 @@ def all_posts(request):
 
 
 def post_detail(request, slug):
-    """ A view to show individual post details """
-
+    template_name = 'blog/post_detail.html'
     post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
 
-    context = {
-        'post': post,
-    }
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
-    return render(request, 'blog/post_detail.html', context)
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})

24639c8145ea2b427d25d8158f0868b8b8f13007
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-17 13:20:28 +0000

    EmailJS functionality added to contact form, all working correctly

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,26 @@ $(document).ready(function () {
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 });
+
+// Code taken from EmailJS.com with small personal amendments
+// EmailJS sendmail function with 'service ID', 'template ID', 'template parameters'
+// Template parameters linked to name attributes in form template
+function sendMail(contactForm) {
+  emailjs
+    .send("lucyjpjones@gmail.com", "fscr", { 
+      fname: contactForm.fname.value,
+      lname: contactForm.lname.value,
+      from_email: contactForm.email.value,
+      topic: contactForm.topic.value,
+      message: contactForm.message.value,
+    })
+    .then(
+      function (response) {
+        alert("MESSAGE SENT", response); // If message is sent successfully, alert "message sent"
+      },
+      function (error) {
+        alert("SENDING FAILED", error); // If message fails to send, alert "sending failed"
+      }
+    );
+  return false; // To block from loading a new page
+}

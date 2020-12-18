a69bbd353dc174c9c9a14f30c3eafff130289e11
Lucyjpjones <lucyjpjones@gmail.com>
2020-12-18 20:02:03 +0000

    js added to close sidenav on click outside menu

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,21 @@ $(document).ready(function () {
     $("#sidebarCollapse").on("click", function () {
         $("#sidebar").toggleClass("active");
         $(".overlay").toggleClass("enabled");
-    });
-
-    /* Menu icon change (toggle) - Code taken from 'https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close' */
-    $("#sidebarCollapse").on("click", function () {
+        /* Menu icon change (toggle) - Code taken from 'https://stackoverflow.com/questions/28247310/bootstrap-mobile-menu-icon-change-to-x-close' */
         $("#navbar-bars").toggleClass("d-none");
         $("#navbar-close").toggleClass("d-none");
     });
+
+
+/* Close Sidenav when click outside area */
+$(document).on("click", function(event) {
+    if (!$(event.target).closest("#sidebarCollapse").length) {
+        $("#sidebar").toggleClass("active");
+        $(".overlay").toggleClass("enabled");
+        $("#navbar-bars").toggleClass("d-none");
+        $("#navbar-close").toggleClass("d-none");
+    }
+});
 
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;

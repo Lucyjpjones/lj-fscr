24803341e7fde6c0c3194f6f83e14136abfefead
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-04 10:18:08 +0000

    rating added to programme and product detail, decimal place changed to 1 in models

--- programmes/models.py
+++ programmes/models.py
@@ -24,7 +24,7 @@ class Programme(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+        max_digits=6, decimal_places=1, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 

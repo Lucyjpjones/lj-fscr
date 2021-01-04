e0a9d3662ca281ceb90999a4dbd7b0d1d07d9ba5
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-04 16:44:33 +0000

    blog posts added to homepage, Post model added to view, filters added to index

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    posts = Post.objects.filter(status=1).order_by('-created_on')[:2]
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'home/index.html', context)
 
 
 def about(request):

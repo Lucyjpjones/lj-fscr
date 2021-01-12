ee486eaa7b5a478cb026a0d7433c949126027ab0
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-12 15:02:07 +0000

    search template add with view and urls, functionality working for products in html

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('search_results', views.search_results, name='search_results'),
     path('meet_the_coaches', views.meet_the_coaches, name='meet_the_coaches'),
     path('about', views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from blog.models import Post
+from products.models import Product
+from programmes.models import Programme
+from django.db.models import Q
+from django.contrib import messages
+from itertools import chain
 
 
 def index(request):
@@ -11,6 +16,35 @@ def index(request):
     }
 
     return render(request, 'home/index.html', context)
+
+
+def search_results(request):
+    """ A view to return the search page """
+
+    # search bar queries
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request,
+                           "You didn't enter any search criteria!")
+            return redirect(reverse('search_results'))
+
+        products = Product.objects.filter(Q(name__icontains=query) | Q(
+                                        description__icontains=query))
+        programmes = Programme.objects.filter(Q(name__icontains=query) | Q
+                                               (description__icontains=query))
+        posts = Post.objects.filter(Q(title__icontains=query))
+
+        results_list = list(chain(products, programmes, posts))
+
+    context = {
+        'search_term': query,
+        'results_list': results_list,
+    }
+
+    return render(request, 'home/search.html', context)
 
 
 def about(request):

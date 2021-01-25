31ef2239238866d025b7e33a878969a939b60472
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-25 17:47:20 +0000

    filter and sort functionality working, sort changed to selector

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
 
@@ -26,7 +25,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':

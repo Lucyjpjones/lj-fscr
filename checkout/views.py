4624b2d7072dd5d244ce9ad5e80ff497b54b17a0
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-26 17:42:49 +0000

    added in code that was deleted by accident

--- checkout/views.py
+++ checkout/views.py
@@ -190,9 +190,10 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    profile = UserProfile.objects.get(user=request.user)
-    order.user_profile = profile
-    order.save()
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
+        order.save()
 
     if save_info:
         profile_data = {

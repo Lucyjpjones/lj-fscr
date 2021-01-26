75155fa5db2f60e578681ab4a2a7019ecce2c2e0
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-26 11:33:30 +0000

    removed unwanted code

--- static/js/script.js
+++ static/js/script.js
@@ -17,11 +17,6 @@ $(document).ready(function () {
         $("#navbar-close").addClass("d-none");
     });
 
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 });
 
 // Code taken from EmailJS.com with small personal amendments

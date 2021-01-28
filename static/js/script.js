d937970a18a29f383f5929ebfdb992f15a2d9498
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-28 14:14:31 +0000

    moved callout img back to html and updated css, moved js toast and emailjs to js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () { 
 
+    /* Toast Messages */
+    $('.toast').toast('show');
+
     /* Sidebar Toggle */
     $("#sidebarCollapse").on("click", function () {
         $("#sidebar").toggleClass("active");
@@ -22,6 +25,11 @@ $(document).ready(function () {
 // Code taken from EmailJS.com with small personal amendments
 // EmailJS sendmail function with 'service ID', 'template ID', 'template parameters'
 // Template parameters linked to name attributes in form template
+
+(function(){
+    emailjs.init("user_qZ2fQYOyvPucFHRyubh33");
+})();
+
 function sendMail(contactForm) {
   emailjs
     .send("lucyjpjones@gmail.com", "fscr", { 

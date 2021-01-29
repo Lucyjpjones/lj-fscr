d4ce9d9399e5edfbcc01458c8c9d54200a53920c
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-29 10:32:15 +0000

    html formatter, pep8 checked

--- profiles/tests.py
+++ profiles/tests.py
@@ -7,14 +7,14 @@ from django.test.client import Client
 class TestProfileViews(TestCase):
 
     def setUp(self):
-        ''' create user '''
+        """ create user """
         self.client = Client()
         self.user = User.objects.create_user('david',
                                              'dbeckham@fscr.com',
                                              'userpassword')
 
     def test_get_profile(self):
-        ''' test profile view, with logged in user '''
+        """ test profile view, with logged in user """
         self.client.login(username='david', password='userpassword')
         response = self.client.get(reverse('profile'))
         self.assertEqual(response.status_code, 200)

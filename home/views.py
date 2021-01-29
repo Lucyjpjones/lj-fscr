3c2b151da0cac265535af55c8651a8f16c9f5f7b
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-29 14:41:39 +0000

    code added so emailjs form is prefilled when a user is logged in

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from blog.models import Post
 from products.models import Product
 from programmes.models import Programme
+from profiles.models import UserProfile
 from django.db.models import Q
 from django.contrib import messages
 from itertools import chain
@@ -12,8 +13,22 @@ def index(request):
     A view to return the index page
     """
     posts = Post.objects.filter(status=1).order_by('-created_on')[:2]
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        first_name = profile.default_full_name.split(" ")[0]
+        last_name = profile.default_full_name.split(" ")[1]
+    else:
+        profile = ''
+        first_name = ''
+        last_name = ''
+
+
     context = {
         'posts': posts,
+        'profile': profile,
+        'first_name': first_name,
+        'last_name': last_name,
     }
 
     return render(request, 'home/index.html', context)

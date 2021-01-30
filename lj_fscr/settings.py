98959998f3c58494fd53df74235d68aec77f0d3f
Lucyjpjones <lucyjpjones@gmail.com>
2021-01-30 16:28:56 +0000

    general save

--- lj_fscr/settings.py
+++ lj_fscr/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+
 ALLOWED_HOSTS = ['lj-fscr.herokuapp.com', 'localhost']
 
 

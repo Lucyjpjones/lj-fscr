f664a5364288ad2aaaba51c5e229bf4f35f9fb22
Lucyjpjones <lucyjpjones@gmail.com>
2021-02-02 07:49:27 +0000

    changed to datetime then changed back

--- checkout/models.py
+++ checkout/models.py
@@ -28,7 +28,7 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True, null=True)
     delivery_cost = models.DecimalField(max_digits=6,
                                         decimal_places=2, null=False,
                                         default=0)

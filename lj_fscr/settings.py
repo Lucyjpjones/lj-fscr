9d1e983aaacaa316ce42c981d4e3e195259c4059
Lucyjpjones <lucyjpjones@gmail.com>
2021-02-03 14:56:21 +0000

    comment added for social login's

--- lj_fscr/settings.py
+++ lj_fscr/settings.py
@@ -124,6 +124,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
+# Social Account login's
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
